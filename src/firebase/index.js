42dc6473c9cde7216695b67ef1c036272a489ffa
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2021-10-25 05:23:23 +0530

    initial commit

--- src/firebase/index.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyB8yOEJHd_3C2nCu0L18my44INaKGCHsfg",
-    authDomain: "nixlab-health-care.firebaseapp.com",
-    projectId: "nixlab-health-care",
-    storageBucket: "nixlab-health-care.appspot.com",
-    messagingSenderId: "3510717033",
-    appId: "1:3510717033:web:c881368f3bf15baeccb0f8",
-    measurementId: "G-BM1T0BP4W5"
-};
